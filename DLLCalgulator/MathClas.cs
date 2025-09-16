5e9a74f71264d8e417c43cfdd7921b409c02e6c2
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-09-16 11:21:19 +0200

    Add project files.

---
+++ DLLCalgulator/MathClas.cs
@@ -0,0 +1,54 @@
+﻿namespace DLLCalgulator
+{
+    public class MathClas
+    {
+        public MathClas()
+        {
+            
+        }
+
+        /// <summary>
+        /// Udregner summen af a og b
+        /// </summary>
+        /// <param name="a">Første værdi</param>
+        /// <param name="b">Anden værdi</param>
+        /// <returns>Resultatet</returns>
+        public int Add(int a, int b) 
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Udregner resultatet af a minus b
+        /// </summary>
+        /// <param name="a">Første værdi</param>
+        /// <param name="b">Anden værdi</param>
+        /// <returns>Resultatet</returns>
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// Udregner resultatet af a ganget med b
+        /// </summary>
+        /// <param name="a">Første værdi</param>
+        /// <param name="b">Anden værdi</param>
+        /// <returns>Resultatet</returns>
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// UDregner resultatet af a divideret med b
+        /// </summary>
+        /// <param name="a">Første værdi</param>
+        /// <param name="b">Anden værdi</param>
+        /// <returns>Resultatet</returns>
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
