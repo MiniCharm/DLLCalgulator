a2116952a4b88dee5d154b1fcfa49ea9426ef306
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-09-17 09:56:42 +0200

    Added XML comments and a copy of the test class.

--- DLLCalgulator/MathClas.cs
+++ DLLCalgulator/MathClas.cs
@@ -2,55 +2,63 @@
 {
     public class MathClas
     {
+        /// <summary>
+        /// En tom constructor til MathClas
+        /// </summary>
         public MathClas()
         {
             
         }
 
         /// <summary>
-        /// Udregner summen af a og b
+        /// Metode, som udregner summen af a og b
         /// </summary>
         /// <param name="a">Første værdi</param>
         /// <param name="b">Anden værdi</param>
-        /// <returns>Resultatet</returns>
+        /// <returns>Resultatet som en int/et heltal</returns>
         public int Add(int a, int b) 
         {
             return a + b;
         }
 
         /// <summary>
-        /// Udregner resultatet af a minus b
+        /// Metode, som udregner resultatet af a minus b
         /// </summary>
         /// <param name="a">Første værdi</param>
         /// <param name="b">Anden værdi</param>
-        /// <returns>Resultatet</returns>
+        /// <returns>Resultatet som en int/ et heltal</returns>
         public int Subtract(int a, int b)
         {
             return a - b;
         }
 
         /// <summary>
-        /// Udregner resultatet af a ganget med b
+        /// Metode, som udregner resultatet af a ganget med b
         /// </summary>
         /// <param name="a">Første værdi</param>
         /// <param name="b">Anden værdi</param>
-        /// <returns>Resultatet</returns>
+        /// <returns>Resultatet som en int/et heltal</returns>
         public int Multiply(int a, int b)
         {
             return a * b;
         }
 
         /// <summary>
-        /// UDregner resultatet af a divideret med b
+        /// Metode, som udregner resultatet af a divideret med b
         /// </summary>
         /// <param name="a">Første værdi</param>
         /// <param name="b">Anden værdi</param>
-        /// <returns>Resultatet</returns>
+        /// <returns>Resultatet som en int/et heltal</returns>
         public int Divide(int a, int b)
         {
             return a / b;
         }
 
+        /// <summary>
+        /// Metode, som udregner kvadratroden af et tal
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns> Resultatet som en double</returns>
         public double SquareRoot(int a) 
         {
             return Math.Sqrt(a);
---
+++ TestNr2/Test1.cs
@@ -0,0 +1,99 @@
+﻿using DLLCalgulator;
+
+namespace TestNr2
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [DataTestMethod]
+        [DataRow(4, 5, 9)]
+        [DataRow(3, 8, 11)]
+        [DataRow(0, 9, 9)]
+
+        [TestMethod]
+        public void Add(int a, int b, int forventetRes)
+        {
+            //Arrange
+            MathClas math = new MathClas();
+
+            //Act
+            int res = math.Add(a, b);
+
+            //Assert
+            Assert.AreEqual(res, forventetRes);
+        }
+
+
+        [DataTestMethod]
+        [DataRow(4, 5, -1)]
+        [DataRow(8, 3, 5)]
+        [DataRow(0, 9, -9)]
+        [TestMethod]
+        public void Subtract(int a, int b, int forventetRes)
+        {
+            //Arrange
+            MathClas math = new MathClas();
+
+            //Act
+            int res = math.Subtract(a, b);
+
+            //Assert
+            Assert.AreEqual(res, forventetRes);
+        }
+
+
+        [DataTestMethod]
+        [DataRow(4, 1, 4)]
+        [DataRow(8, 4, 2)]
+        [DataRow(0, 9, 0)]
+        [TestMethod]
+        public void Divide(int a, int b, int forventetRes)
+        {
+            //Arrange
+            MathClas math = new MathClas();
+
+            //Act
+            int res = math.Divide(a, b);
+
+            //Assert
+            Assert.AreEqual(res, forventetRes);
+        }
+
+
+        [DataTestMethod]
+        [DataRow(4, 5, 20)]
+        [DataRow(3, 8, 24)]
+        [DataRow(0, 9, 0)]
+        [TestMethod]
+        public void Multiplyd(int a, int b, int forventetRes)
+        {
+            //Arrange
+            MathClas math = new MathClas();
+
+            //Act
+            int res = math.Multiply(a, b);
+
+            //Assert
+            Assert.AreEqual(res, forventetRes);
+        }
+
+        [DataTestMethod]
+        [DataRow(4, 2)]
+        [DataRow(11, 3.31)]
+        [DataRow(0, 0)]
+        [DataRow(5, 2.23)]
+        [TestMethod]
+        public void SquareRoot(int a, double forventetRes)
+        {
+            //Arrange
+            MathClas math = new MathClas();
+
+            //Act
+            double res = math.SquareRoot(a);
+
+            //Assert
+            Assert.AreEqual(res, forventetRes, 0.01);
+        }
+
+    }
+}
